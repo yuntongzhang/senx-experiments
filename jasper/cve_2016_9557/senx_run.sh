09a3620828e66cb994c0c1d5b0e22e3924368543
Zhang Yuntong <ang.unong@gmail.com>
2022-04-05 22:04:08 +0800

    Fix jasper run file

--- jasper/cve_2016_9557/senx_run.sh
+++ jasper/cve_2016_9557/senx_run.sh
@@ -3,4 +3,4 @@ bin=imginfo
 
 analyze_bc ./$bin.bc 2>analyze.err
 python3 ../../prepare_gdb_def.py $bin $(pwd)
-timeout 1800 senx -struct-def=def_file ./$bin.bc -f ../exploit 2>senx.err
+timeout 1800 senx -struct-def=def_file ./$bin.bc -f ./exploit 2>senx.err
