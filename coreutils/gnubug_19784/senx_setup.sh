57235297c833bdc3afec64d9ca6810295240b5ff
Zhang Yuntong <ang.unong@gmail.com>
2022-04-05 22:16:16 +0800

    Fix 19784 setup script

--- coreutils/gnubug_19784/senx_setup.sh
+++ coreutils/gnubug_19784/senx_setup.sh
@@ -5,7 +5,8 @@ cd source/
 git checkout 658529a
 
 ./bootstrap
-export FORCE_UNSAFE_CONFIGURE=1 && CC="wllvm" CXX="wllvm++" CFLAGS="-g -O0" CXXFLAGS="$CFLAGS"./configure
+export FORCE_UNSAFE_CONFIGURE=1 && CC="wllvm" CXX="wllvm++" CFLAGS="-g -O0" CXXFLAGS="$CFLAGS" ./configure
+
 CC="wllvm" CXX="wllvm++" make CFLAGS="-g -O0 -static" CXXFLAGS="$CFLAGS" src/make-prime-list
 
 bin=make-prime-list
