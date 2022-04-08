91dbe6a9f1913337824e63dde4c75be1afba761c
Zhang Yuntong <ang.unong@gmail.com>
2022-04-08 16:10:03 +0800

    Further update cve_2018_14498 setup script

--- libjpeg/cve_2018_14498/senx_setup.sh
+++ libjpeg/cve_2018_14498/senx_setup.sh
@@ -3,7 +3,7 @@ git clone https://github.com/libjpeg-turbo/libjpeg-turbo.git source
 cd source/
 git checkout ae8cdf5
 
-
+autoreconf -i
 CC="wllvm" CXX="wllvm++" ./configure CFLAGS="-g -O0 -static" CXXFLAGS="$CFLAGS" --without-simd --enable-static --disable-shared
 make CFLAGS="-g -O0 -static" CXXFLAGS="$CFLAGS" -j`nproc`
 
