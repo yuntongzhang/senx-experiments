be9b57a8f5a4923dbc911a1f42668ca3ab2d662c
Zhang Yuntong <ang.unong@gmail.com>
2022-05-07 16:24:04 +0800

    Remove previous results and redundant files

--- jasper/cve_2016_9557/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-unzip source.zip
-cd source/
-
-autoreconf -i
-./configure
-make CFLAGS="-static -g -fsanitize=address -fsanitize=undefined" CXXFLAGS="-static -g -fsanitize=address -fsanitize=undefined" LDFLAGS="-fsanitize=address -fsanitize=undefined" -j10
-
-cp src/appl/imginfo ../
